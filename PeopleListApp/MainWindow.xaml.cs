052993929283bc0d6884e1fe1700453bc44e6592
Woozej <krystian__1993@o2.pl>
2022-02-20 17:08:12 +0100

    Add project files.

---
+++ PeopleListApp/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace PeopleListApp
+{
+    public partial class MainWindow : Window
+    {
+        //Data inside the table
+        ObservableCollection<Person> people = new ObservableCollection<Person>();
+        //Flag to check if buttons are disabled
+        bool buttonsDisabled = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadPeople();
+            SaveTableButton.Click += SaveTableButton_Click;
+            DiscardChangesButton.Click += DiscardChangesButton_Click;
+        }
+
+        //Add button event
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            people.Add(new Person("FirstName", "LastName", "StreetName", "HouseNumber", null, "PostalCode", "Town", "000000000", new DateTime(2000, 1, 1)));
+            DisableButtons(false);
+        }
+
+        //Delete button event
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            people.Remove((Person)PeopleDataGrid.SelectedItem);
+            DisableButtons(false);
+        }
+
+        //Save button event
+        private void SaveTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            //checking if provided data is valid
+            StringBuilder validationMassage = new StringBuilder();
+            foreach (var person in people)
+            {
+                validationMassage.Append(person.Validate());
+            }
+            if (validationMassage.Length > 0)
+            {
+                validationMassage.Append("Please fix this issues before saving");
+                MessageBox.Show(validationMassage.ToString(), "Incorrect data");
+                return;
+            }
+
+            //Writing to file
+            List<Person> listOfPeople = people.ToList();
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(List<Person>));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//ListOfPeople.xml";
+            FileStream file = File.Create(path);
+            writer.Serialize(file, listOfPeople);
+            file.Close();
+            DisableButtons(true);
+        }
+
+        //Cancel button event
+        private void DiscardChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPeople();
+            DisableButtons(true);
+        }
+
+        //Function to load people from file
+        private void LoadPeople()
+        {
+            people.Clear();
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(List<Person>));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//ListOfPeople.xml";
+
+            //If there is no file in path list remains empty
+            if (File.Exists(path))
+            {
+                StreamReader file = new StreamReader(path);
+
+                List<Person> listOfPeople = (List<Person>)reader.Deserialize(file);
+
+                people = new ObservableCollection<Person>(listOfPeople);
+            }
+            PeopleDataGrid.ItemsSource = people;
+        }
+
+        //Event after which data is reloaded and buttons became active
+        private void PeopleDataGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new DispatcherOperationCallback(param =>
+            {
+                DisableButtons(false);
+                CollectionViewSource.GetDefaultView(people).Refresh();
+                return null;
+            }), DispatcherPriority.Background, new object[] { null });
+        }
+
+        //Event after which buttons became active
+        private void PeopleDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                DisableButtons(false);
+            }
+        }
+
+        //Event after which buttons became active
+        private void BirthDateDatePicker_CalendarClosed(object sender, RoutedEventArgs e)
+        {
+            DisableButtons(false);
+        }
+
+        //Function to disable/enable Save and Cancel Button
+        private void DisableButtons(bool disable) 
+        {
+            if (disable)
+            {
+                if (buttonsDisabled)
+                    return;
+                else 
+                {
+                    SaveTableButton.IsEnabled = false;
+                    DiscardChangesButton.IsEnabled = false;
+                    buttonsDisabled = true;
+                }
+            }
+            else 
+            {
+                if (!buttonsDisabled)
+                    return;
+                else
+                {
+                    SaveTableButton.IsEnabled = true;
+                    DiscardChangesButton.IsEnabled = true;
+                    buttonsDisabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ PeopleListApp/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+namespace PeopleListApp
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public string HouseNumber { get; set; }
+        public int? ApartmentNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Town { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                _dateOfBirth = value;
+                //Calculating age and assigning that variable to _age
+                int age = DateTime.Now.Year - _dateOfBirth.Year;
+                if (DateTime.Now.Month < _dateOfBirth.Month || (DateTime.Now.Month == _dateOfBirth.Month && DateTime.Now.Day < _dateOfBirth.Day))
+                    age--;
+
+                _age = age;
+
+            }
+
+        }
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+        }
+
+        public Person()
+        { }
+
+        public Person(string firstName, string lastName, string streetName, string houseNumber, int? apartmentNumber, string postalCode, string town, string phoneNumber, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+            ApartmentNumber = apartmentNumber;
+            PostalCode = postalCode;
+            Town = town;
+            PhoneNumber = phoneNumber;
+            DateOfBirth = dateOfBirth;
+        }
+
+        //Function to check if provided data is valid
+        public StringBuilder Validate()
+        {
+            StringBuilder result = new StringBuilder();
+            if (string.IsNullOrEmpty(FirstName))
+                result.Append("First name is empty.\n");
+            if (FirstName == "FirstName")
+                result.Append("First name have default value.\n");
+
+            if (string.IsNullOrEmpty(LastName))
+                result.Append("Last name is empty.\n");
+            if (LastName == "LastName")
+                result.Append("Last name have default value.\n");
+
+            if (string.IsNullOrEmpty(StreetName))
+                result.Append("Street name is empty.\n");
+            if (StreetName == "StreetName")
+                result.Append("Street name have default value.\n");
+
+            if (string.IsNullOrEmpty(HouseNumber))
+                result.Append("House number is empty.\n");
+            if (HouseNumber == "HouseNumber")
+                result.Append("House number have default value.\n");
+
+            if (string.IsNullOrEmpty(PostalCode))
+                result.Append("Postal code is empty.\n");
+            if (PostalCode == "PostalCode")
+                result.Append("Postal code have default value.\n");
+
+            if (string.IsNullOrEmpty(Town))
+                result.Append("Town is empty.\n");
+            if (Town == "Town")
+                result.Append("Town have default value.\n");
+
+            if (string.IsNullOrEmpty(PhoneNumber))
+                result.Append("PhoneNumber is empty.\n");
+            if (PhoneNumber == "000000000")
+                result.Append("PhoneNumber have default value.\n");
+
+            if (_dateOfBirth > DateTime.Now)
+                result.Append("Person can't be born in future.\n");
+
+            if (result.Length > 0)
+                result.Insert(0, "For user with first name '" + FirstName + "' the following issues were detecteted:\n");
+
+            return result;
+        }
+    }
+}
